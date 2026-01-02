74fafec19acb0d6b79f6afba15d4463704f0440a
EngSalem <mhmd.sl.elhady@gmail.com>
2026-01-01 21:49:19 -0500

    add causal attention implementation

---
+++ building_llms_scratch/chapter 3/self attention/self_attention_causal.py
@@ -0,0 +1,41 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+class selfAttentionCausal(nn.Module):
+    def __init__(self, dim_in, dim_out,seq_len, qwv_bias=False):
+        super(selfAttentionCausal, self).__init__()
+        self.dim_in = dim_in
+        self.dim_out = dim_out
+        # identify query, key, value weight matrices
+        self.q = nn.Linear(dim_in, dim_out, bias=qwv_bias)
+        self.k = nn.Linear(dim_in, dim_out, bias=qwv_bias)
+        self.v = nn.Linear(dim_in, dim_out, bias=qwv_bias)
+        self.dropout = nn.Dropout(0.1)
+        self.register_buffer("mask", torch.tril(torch.ones((seq_len, seq_len))).unsqueeze(0))
+
+    def forward(self, x):
+        # x_shape: (batch_size, seq_length, dim_in)
+        batch_size, seq_length, dim_in = x.shape # B x S x dim_in for debugging not used in implementation
+
+        q_w = self.q(x) # B x S x dim_out
+        k_w = self.k(x) # B x S x dim_out
+        v_w = self.v(x) # B x S x dim_out
+
+        # dot product queries and keys to get raw attention scores
+        attn_scores = q_w @ k_w.transpose(1, 2) # B x S x S
+        
+        attn_scores = attn_scores.masked_fill(self.mask[:, :seq_length, :seq_length] == 0, float('-inf')) # B
+
+        # apply softmax to get attention weights
+        attn_weights = F.softmax(attn_scores/k_w.shape[-1]**0.5, dim = -1) # B x S x S
+
+        # apply dropout
+        attn_weights = self.dropout(attn_weights)
+
+        # multiply attention weights with values to get output vectors
+
+        out = attn_weights @ v_w # B x S x dim_out
+        return out
+
+
